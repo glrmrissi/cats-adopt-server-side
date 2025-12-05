9ca99e28f54332852a1c5dd090fa96ac2f3763f6
glrmrissi <guilhermerissi04112006@gmail.com>
2025-12-05 19:53:55 -0300

    refactor: remove unused methods and simplify AppController and AppService

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { IsPublic } from './decorators/is-public.decorator';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-  @IsPublic()
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor() {}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,32 @@ import { AuthGuard } from './guards/auth/auth.guard';
 import { APP_GUARD } from '@nestjs/core';
 import { AppService } from './app.service';
 import { Logger, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { LoginModule } from './login/login.module';
 
 @Module({
-  imports: [LoginModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: 'cats-adopt',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123',
+      autoLoadEntities: true,
+      // synchronize: true,
+    }),
+
+    LoginModule
+  ],
   controllers: [AppController],
   providers: [
     {
-      provide: APP_GUARD,
+      provide: APP_GUARD, 
       useClass: AuthGuard,
     },
     AppService,
-    Logger,
+    Logger
   ],
 })
 export class AppModule { }
